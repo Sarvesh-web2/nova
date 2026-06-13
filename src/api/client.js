cd6636be6d8d9862325cc7557a45ca18a3401a0a
pmkesavan147 <pmkesavan147@gmail.com>
2026-06-13 23:49:11 +0530

    Updated frontend

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE = "http://localhost:8002";
 
 async function request(path, options) {
   const res = await fetch(`${API_BASE}${path}`, {
